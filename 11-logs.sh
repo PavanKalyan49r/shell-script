b2c847d384404e05be821cde5e1e072deba3679c
PavanKalyan49r <pavan49r@gmail.com>
2023-12-29 21:00:36 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -14,19 +14,19 @@ echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-echo "error :: $2 ... $R failed $N"
+echo -e "error :: $2 ... $R failed $N"
 exit 1
 else
-echo "$2 ... $G success $N"
+echo -e "$2 ... $G success $N"
 fi
 }
 
 if [ $ID -ne 0 ]
 then
-echo "$R error :: please run this script with root access $N"
+echo -e "$R error :: please run this script with root access $N"
 exit 1 #you can give other than 0
 else
-echo "you are root user"
+echo -e "you are root user"
 fi # fi means reverse of if, indicating condition end
 
 yum install mysql -y &>> $LOGFILE
